e192f7508cfdeed87b53a7a2de153d8beeeaf8b5
jverce <juanjov@gmail.com>
2019-06-06 23:05:17 -0700

    Add Union-Find functions for Problem 1

--- course3/assignments/src/week2/prob_1.clj
+++ course3/assignments/src/week2/prob_1.clj
@@ -1,3 +1,32 @@
 (ns week2.prob_1)
 
-(def edges (common.utils/data-loader "resources/clustering1.txt"))
+(require '[common.utils :as u])
+(require '[week1.prob-3 :as w1p3])
+
+(defn find-uf
+  [sets x]
+  (first
+   (filter #(contains? % x) sets)))
+
+(defn find-uf-complement
+  [sets & xs]
+  (clojure.set/difference sets (set (map #(find-uf sets %) xs))))
+
+(defn union-uf
+  [sets & xs]
+  (conj
+   (apply find-uf-complement sets xs)
+   (apply clojure.set/union
+          (map #(find-uf sets %) xs))))
+
+(def edges-data
+  (w1p3/sort-by-weight
+   (u/data-loader "resources/week2/input_completeRandom_10_32.txt")))
+
+(defn assoc-points
+  [clusters e]
+  union-uf (:groups clusters) )
+
+(defn clusters
+  [k]
+  ())
