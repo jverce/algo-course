21352e7438948e23aa3e310a5a6dee59bfb735c6
jverce <juanjov@gmail.com>
2019-06-08 09:33:49 -0700

    Add missing doc to week2 code

--- course3/assignments/src/week2/prob_1.clj
+++ course3/assignments/src/week2/prob_1.clj
@@ -1,28 +1,40 @@
 (ns week2.prob-1)
 
-(require '[common.utils :as u])
-(require '[week1.prob-3 :as w1p3])
+(require '[common.utils :as u]
+         '[week1.prob-3 :as w1p3])
 
+;; Load input data.
 (def edges-data
   (w1p3/sort-by-weight
    (u/data-loader "resources/week2/clustering1.txt")))
 
+;; Create the Union-Find structure for the input edges data.
 (defn initial-cluster
   [edges]
-  (set (map
-        #(identity #{%})
-        (into nil (w1p3/vertices-of-edge-records edges)))))
+  (set
+   (u/create-uf (into nil (w1p3/vertices-of-edge-records edges)))))
 
+;; Creates the initial hash-map structure to use for
+;; repetitive computations:
+;; - `:groups` refers to the Union-Find structure outlining the
+;;             clustering.
+;; - `:distance` refers to the max distance for this particular
+;;               clustering.
 (defn initial-map
   [edges]
   {:groups (initial-cluster edges) :distance 0})
 
+;; Given a cluster map `clusters` and a new edge, perform
+;; the clustering computations for the vertices of such edge.
 (defn assoc-points
   [clusters e]
   (let [new-groups (apply u/union-uf (:groups clusters) (w1p3/vertices e))]
     {:groups new-groups
      :distance (w1p3/weight e)}))
 
+;; Compute the k-cluster of the list of edges `edges`.
+;; This list must be sorted in ascending cost order for this
+;; function to compute the max distance clustering.
 (defn clusters
   [edges k]
   (first (drop-while
--- course3/assignments/src/week2/prob_2.clj
+++ course3/assignments/src/week2/prob_2.clj
@@ -1,4 +1,37 @@
 (ns week2.prob-2)
 
-(require '[common.utils :as u])
-(require '[week1.prob-3 :as w1p3])
+(require '[common.utils :as u]
+         '[week1.prob-3 :as w1p3]
+         '[week2.prob-1 :as w2p1])
+
+;; Load input data
+(def edges-data
+  (u/data-loader "resources/week2/clustering_big.txt"))
+
+;; Transforms a list of bits (represented as `"1"` or `"0"`)
+;; into its corresponding base 10 integer.
+(defn binary-list-to-dec
+  [binlist]
+  (Integer/parseInt (clojure.string/join binlist) 2))
+
+;; Takes the input list of implicit edge costs (provided in the input)
+;; and converts it to their base 10 integer representation.
+(defn edges-data-as-ints
+  [edges]
+  (map binary-list-to-dec edges))
+
+;; Transform an input list of edge costs into
+;; a map that associates each vertex ID with its
+;; corresponding edge cost (vertices ID's are assumed
+;; to be determined by the position in the input list).
+(defn edge-costs-to-vertices-assoc
+  [edges]
+  (let [enumerated-entries (map-indexed #(identity {%1 %2}) edges)]
+    (reduce #(conj %1 %2) {} enumerated-entries)))
+
+;; Create a Union-Find for the list of vertices corresponding
+;; to the input list `edges` (assumed implicitly based on their
+;; position in the list).
+(defn create-uf-for-vertices
+  [edges]
+  (u/create-uf (range 0 (count edges))))
