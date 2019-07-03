b865731dfa442c9927aa817712dbdcabc7300c99
jverce <juanjov@gmail.com>
2019-07-03 03:39:06 -0700

    Completed Knapsack problem

--- course3/assignments/src/week4/prob_1_2.clj
+++ course3/assignments/src/week4/prob_1_2.clj
@@ -17,3 +17,46 @@
 (defn knapsack-items
   [data]
   (rest data))
+
+(defn item-value
+  [item]
+  (first item))
+
+(defn item-weight
+  [item]
+  (last item))
+
+(def x-range
+  (memoize
+   (fn [data]
+     (range 0 (inc (knapsack-weight data))))))
+
+(defn mask-first-n
+  [xs n]
+  (let [size (count xs)
+        take-size (- size n)]
+    (concat (replicate n 0) (take take-size xs))))
+
+(defn knapsack
+  [data]
+  (loop [items (knapsack-items data)
+         accum (replicate (knapsack-weight data) 0)]
+    (if (empty? items)
+      accum
+      (let [xs (x-range data)
+            item (first items)
+            vi (item-value item)
+            wi (item-weight item)
+            accum-with-new-item (map #(+ % vi) accum)
+            accum-shifted (mask-first-n accum-with-new-item wi)]
+        (recur (rest items)
+               (map max accum accum-shifted))))))
+
+(defn knapsack-value
+  [data]
+  (last (knapsack data)))
+
+(defn prob1 []
+  (knapsack-value (data "resources/week4/knapsack1.txt")))
+(defn prob2 []
+  (knapsack-value (data "resources/week4/knapsack_big.txt")))
