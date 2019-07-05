e42170e4d68aff17dc0206ed8b1ddd6cc56c585e
jverce <juanjov@gmail.com>
2019-07-04 21:12:43 -0700

    Completed Knapsack problem

---
+++ course3/assignments/src/week4/core.clj
@@ -0,0 +1,10 @@
+(ns week4.core
+  (:gen-class))
+
+(require '[week4.prob-1-2 :as p1p2])
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (p1p2/prob1))
+  (println (p1p2/prob2)))
--- course3/assignments/src/week4/prob_1_2.clj
+++ course3/assignments/src/week4/prob_1_2.clj
@@ -18,31 +18,44 @@
   [data]
   (rest data))
 
+;; Given an item `item` as extracted from a Knapsasck
+;; input file, return its value.
 (defn item-value
   [item]
   (first item))
 
+;; Given an item `item` as extracted from a Knapsasck
+;; input file, return its weight.
 (defn item-weight
   [item]
   (last item))
 
+;; Return a list of consecutive weights, from 0
+;; to the total weight of the Knapsack. `data`
+;; represents the whole input for the Knapsack problem.
 (def x-range
   (memoize
    (fn [data]
      (range 0 (inc (knapsack-weight data))))))
 
+;; This function takes a list `xs` and generates
+;; a new list of the same size, with its first `n`
+;; elements _shifted_ to the right. The first `n`
+;; elements of the new list are set to 0.
 (defn mask-first-n
   [xs n]
   (let [size (count xs)
         take-size (- size n)]
-    (concat (replicate n 0) (take take-size xs))))
+    (concat (repeat n 0) (take take-size xs))))
 
+;; Return the optimal value of the Knapsack problem
+;; for the input `data`.
 (defn knapsack
   [data]
   (loop [items (knapsack-items data)
-         accum (replicate (knapsack-weight data) 0)]
+         accum (repeat (knapsack-weight data) 0)]
     (if (empty? items)
-      accum
+      (last accum)
       (let [xs (x-range data)
             item (first items)
             vi (item-value item)
@@ -52,11 +65,7 @@
         (recur (rest items)
                (map max accum accum-shifted))))))
 
-(defn knapsack-value
-  [data]
-  (last (knapsack data)))
-
 (defn prob1 []
-  (knapsack-value (data "resources/week4/knapsack1.txt")))
+  (knapsack (data "resources/week4/knapsack1.txt")))
 (defn prob2 []
-  (knapsack-value (data "resources/week4/knapsack_big.txt")))
+  (knapsack (data "resources/week4/knapsack_big.txt")))
