8f4f42137bcf4a0720f6d7ab05b2fb43b680573b
Jay Vercellone <juanjov@gmail.com>
2020-03-04 23:45:44 -0800

    Implement BF algorithm
    * Implement BF optimization function.
    * Iterate (n - 1) times to get the final BF result.

--- course4/src/week1/bellman_ford.rs
+++ course4/src/week1/bellman_ford.rs
@@ -1,5 +1,6 @@
 use crate::common::utils::{to_indeg_edges, vertices};
 use crate::week1::types;
+use std::cmp::min;
 use std::collections::HashMap;
 use std::i64::MAX;
 
@@ -7,8 +8,14 @@ type ShortestPaths = HashMap<u64, i64>;
 
 /// Computes the optimization function for the Bellman-Ford
 /// algorithm.
-fn opt(prev: i64, indeg: &[&types::Edge]) -> i64 {
-    return prev;
+fn opt(prev: i64, indeg_prev: &[i64], indeg: &[&types::Edge]) -> i64 {
+    let n_indeg = indeg_prev.len();
+    let mut min_indeg = prev;
+    for i in 0..n_indeg {
+        let new_cost = indeg[i].weight.saturating_add(indeg_prev[i]);
+        min_indeg = min(min_indeg, new_cost);
+    }
+    return min_indeg;
 }
 
 /// Computes a table with the shortest-path distances from vertex `s`
@@ -18,18 +25,28 @@ fn opt(prev: i64, indeg: &[&types::Edge]) -> i64 {
 /// vertex `s` to said destination vertex.
 pub fn solve(s: u64, g: Vec<types::Edge>) -> Option<ShortestPaths> {
     let n = g.len();
-    let vs = vertices(g.as_slice());
-    let indeg = to_indeg_edges(g.as_slice());
+    let vs = vertices(&g);
+    let indeg = to_indeg_edges(&g);
     let mut result: ShortestPaths = vs
         .iter()
         .map(|&v| if v == s { (v, 0) } else { (v, MAX) })
         .collect();
 
     for _ in 1..n {
-        result
-            .iter_mut()
-            .for_each(|(k, v)| *v = opt(*v, indeg[k].as_slice()));
+        let old_result: ShortestPaths = result.clone();
+        result = result
+            .iter()
+            .map(|(k, v)| {
+                let indeg = indeg[k].as_slice();
+                let indeg_prev: Vec<i64> = indeg.iter().map(|&v| result[&v.tail]).collect();
+                return (*k, opt(*v, &indeg_prev, indeg));
+            })
+            .collect();
+
+        if old_result == result {
+            break;
+        }
     }
 
-    return None;
+    return Some(result);
 }
