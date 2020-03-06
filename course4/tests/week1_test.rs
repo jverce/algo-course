1047e35ad2c47fe597b3276821f3babb6cdb6b03
Jay Vercellone <juanjov@gmail.com>
2020-03-06 00:34:26 -0800

    Scan test cases dir

--- course4/tests/week1_test.rs
+++ course4/tests/week1_test.rs
@@ -1,7 +1,17 @@
 extern crate course4;
 use course4::week1::solution::solve_for_file;
+use std::fs::read_dir;
 
 #[test]
 fn solution_is_computed_correctly() {
+    let dir_name = "resources/week1/test_cases";
+    let result = read_dir(dir_name);
+    match result {
+        Ok(files) => files.for_each(|f| {
+            let filename = f.unwrap().file_name();
+            println!("{:?}", filename);
+        }),
+        Err(_) => println!("Directory not found: {}", dir_name),
+    }
     assert_eq!(2, 2);
 }
