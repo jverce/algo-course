31a1b3ccb9a1d5d7f0288e5a1fe93811366d8af5
jverce <juanjov@gmail.com>
2020-03-11 15:51:38 -0700

    Parallelize BF computing by source

--- course4/src/week1/solution.rs
+++ course4/src/week1/solution.rs
@@ -1,5 +1,7 @@
 use std::cmp::min;
 
+use rayon::prelude::*;
+
 use crate::common::utils::{read_lines, to_edges, vertices};
 use crate::week1::bellman_ford;
 use crate::week1::types::ShortestPaths;
@@ -10,7 +12,7 @@ pub fn solve_for_file(filename: &str) -> Option<i64> {
     let file_content = read_lines(filename);
     let edges = to_edges(file_content);
     let sources = vertices(&edges);
-    let results: Vec<Option<ShortestPaths>> = sources.iter().map(|s| bellman_ford::solve(*s, &edges)).collect();
+    let results: Vec<Option<ShortestPaths>> = sources.par_iter().map(|s| bellman_ford::solve(*s, &edges)).collect();
 
     let mut result = None;
     for r in results {
