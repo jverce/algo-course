2bb7f80e7731ebe83205ac39b6a7699b934e8067
Jay Vercellone <juanjov@gmail.com>
2020-03-14 00:08:53 -0700

    Add benchmark for FW algorithm

--- course4/benches/week1.rs
+++ course4/benches/week1.rs
@@ -2,15 +2,15 @@ extern crate course4;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use course4::week1::solution::solve_for_file_bf;
+use course4::week1::solution::{solve_for_file_bf, solve_for_file_fw};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let test_filename = "resources/week1/test_cases/input_random_25_128.txt";
     c.bench_function("BF", |b| {
-        b.iter(|| {
-            solve_for_file_bf(black_box(
-                "resources/week1/test_cases/input_random_25_128.txt",
-            ))
-        })
+        b.iter(|| solve_for_file_bf(black_box(test_filename)))
+    });
+    c.bench_function("FW", |b| {
+        b.iter(|| solve_for_file_fw(black_box(test_filename)))
     });
 }
 
