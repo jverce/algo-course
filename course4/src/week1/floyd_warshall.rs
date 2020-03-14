b447bae79d96530d080121999bfcf9ec4589d654
Jay Vercellone <juanjov@gmail.com>
2020-03-13 20:19:33 -0700

    Add Floyd-Warshall algorithm solution

---
+++ course4/src/week1/floyd_warshall.rs
@@ -0,0 +1,54 @@
+use crate::common::utils::{to_outdeg_edges, vertices};
+use crate::week1::types::{Graph, VertexId};
+use std::collections::HashMap;
+use std::i64::MAX;
+
+/// Computes the shortest-path distances of all the pairs
+/// of vertices in the graph `g` using the Floyd-Warshall algorithm
+/// The table is indexed by the source and destination
+/// vertex **IDs** as a tuple `(s, t)`, and the value
+/// associated to it is the length of the shortest path from the
+/// vertex `s` to the destination vertex `t`.
+pub fn solve(g: &Graph) -> HashMap<(VertexId, VertexId), i64> {
+    let vs = vertices(&g);
+    let n = vs.len();
+    let outdeg = to_outdeg_edges(&g);
+    let mut path_weights: HashMap<(VertexId, VertexId), i64> = HashMap::new();
+
+    for i in 1..=n {
+        let key = (i, i);
+        path_weights.insert(key, 0);
+    }
+
+    for (i, edges) in outdeg {
+        for e in edges {
+            let key = (i, e.head);
+            let curr_weight = path_weights.get(&key).or(Some(&MAX)).unwrap();
+            if curr_weight > &e.weight {
+                path_weights.insert(key, e.weight);
+            }
+        }
+    }
+
+    for k in 1..=n {
+        for i in 1..=n {
+            for j in 1..=n {
+                let key = (i, j);
+                let curr_weight = path_weights.get(&key).or(Some(&MAX)).unwrap();
+
+                let rhs_key = (i, k);
+                let rhs_weight = path_weights.get(&rhs_key).or(Some(&MAX)).unwrap();
+
+                let lhs_key = (k, j);
+                let lhs_weight = path_weights.get(&lhs_key).or(Some(&MAX)).unwrap();
+
+                let new_weight = rhs_weight.saturating_add(*lhs_weight);
+                if curr_weight > &new_weight {
+                    path_weights.insert(key, new_weight);
+                }
+            }
+        }
+    }
+
+    return path_weights;
+}
--- course4/src/week1/mod.rs
+++ course4/src/week1/mod.rs
@@ -1,3 +1,4 @@
 pub mod bellman_ford;
+pub mod floyd_warshall;
 pub mod solution;
 pub mod types;
