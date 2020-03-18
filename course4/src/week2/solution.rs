8e6111bd774254eb45c63b81b150400ac59d5b16
Jay Vercellone <juanjov@gmail.com>
2020-03-17 23:14:19 -0700

    Add new type for TSP result

--- course4/src/week2/solution.rs
+++ course4/src/week2/solution.rs
@@ -1,1 +1,12 @@
-pub fn solve() {}
+use crate::week2::types::TspResult;
+
+/// Computes the solution to the problem for the file
+/// located at `filename` using the Floyd-Warshall algorithm.
+pub fn solve_for_file(filename: &str) -> TspResult {
+    return 0;
+}
+
+pub fn solve() {
+    let result = solve_for_file("resources/week2/tsp.txt");
+    println!("{}", result);
+}
