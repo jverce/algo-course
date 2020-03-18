af8af62a06349f6897de679aceaa6d185393cb25
Jay Vercellone <juanjov@gmail.com>
2020-03-18 01:04:07 -0700

    Fix comment

--- course4/src/week2/solution.rs
+++ course4/src/week2/solution.rs
@@ -1,8 +1,8 @@
 use crate::common::utils::{read_lines, to_edges_from_xy_position};
 use crate::week2::types::TspResult;
 
-/// Computes the solution to the problem for the file
-/// located at `filename` using the Floyd-Warshall algorithm.
+/// Computes the solution to the TSP problem for the file
+/// located at `filename` using dynamic programming.
 pub fn solve_for_file(filename: &str) -> TspResult {
     let file_contents: Vec<Vec<f64>> = read_lines(filename);
     let edges = to_edges_from_xy_position(file_contents);
