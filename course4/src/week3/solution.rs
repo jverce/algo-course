31747e32d0e174f48a58df1fb3ca948ecafa3023
Jay Vercellone <juanjov@gmail.com>
2020-03-29 02:29:36 -0700

    Put spatial points in an RTree

--- course4/src/week3/solution.rs
+++ course4/src/week3/solution.rs
@@ -1,12 +1,14 @@
 use crate::common::types::{PointVertex, Real};
 use crate::common::utils::{read_lines, to_points};
 use crate::week2::types::TspResult;
+use spade::rtree::RTree;
 use typenum::U2;
 
 pub fn solve_for_file(filename: &str) -> TspResult {
     let file_contents: Vec<Vec<Real>> = read_lines(filename);
     let points: Vec<PointVertex<Real, U2>> = to_points(file_contents);
-    return points.len() as TspResult;
+    let rtree = RTree::bulk_load(points);
+    return rtree.size() as TspResult;
 }
 
 pub fn solve() {
