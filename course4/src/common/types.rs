b077527622d4cea2bc475664fdab8af1be438cfb
Jay Vercellone <juanjov@gmail.com>
2020-03-29 13:37:28 -0700

    Implement traits for `PointVertex`
    Since `PointVertex` contains both a unique ID for each vertex as well
    as spatial data, it is desired to exclude the latter from any
    accidental usage when distinguishing between vertices.

--- course4/src/common/types.rs
+++ course4/src/common/types.rs
@@ -1,7 +1,9 @@
 use generic_array::{ArrayLength, GenericArray};
 use spade::{PointN, SpadeNum, TwoDimensional};
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
 
 pub type VertexId = usize;
 pub type Weight = f64;
@@ -24,14 +26,52 @@ pub type GraphTab = HashMap<(VertexId, VertexId), Weight>;
 /// Represents a point in the space `T^N`.
 pub type Point<T, N> = GenericArray<T, N>;
 
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
+#[derive(Clone, Debug)]
 /// Represents a labeled vertex located at point in the space `T^N`.
 pub struct PointVertex<T, N>
 where
+    T: SpadeNum,
     N: ArrayLength<T>,
 {
     pub point: Point<T, N>,
     pub id: VertexId,
+}
+
+impl<T, N> PartialEq for PointVertex<T, N>
+where
+    T: SpadeNum,
+    N: ArrayLength<T>,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl<T, N> Eq for PointVertex<T, N>
+where
+    T: SpadeNum,
+    N: ArrayLength<T>,
+{
+}
+
+impl<T, N> PartialOrd for PointVertex<T, N>
+where
+    T: SpadeNum,
+    N: ArrayLength<T>,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.id.partial_cmp(&other.id)
+    }
+}
+
+impl<T, N> Hash for PointVertex<T, N>
+where
+    T: SpadeNum,
+    N: ArrayLength<T>,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.id.hash(state)
+    }
 }
 
 impl<T, N> PointN for PointVertex<T, N>
