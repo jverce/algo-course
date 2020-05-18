8d22d6bb4bea2928a139dd05a5019f730d0fb408
Jay Vercellone <juanjov@gmail.com>
2020-05-18 00:54:45 -0700

    Optimizations
    * Improve the `eval` function for `ExprFull` expressions, to keep
      internal count of all unsatisfied terms, and to only re-evaluate the
      terms that are affected during each iteration.
    * Simplify `ExprFull` expressions during construction to remove
      irrelevant terms from it. This made a HUGE difference.
    * Reduce the amount of iterations the program executes, by a factor of N.
    * Remove unused functions in traits.

--- course4/src/week4/solution.rs
+++ course4/src/week4/solution.rs
@@ -2,7 +2,7 @@ use itertools::Itertools;
 use rayon::prelude::*;
 
 use crate::common::utils::read_lines;
-use crate::week4::types::{BitSet, BitVec, Evaluable, ExprFull, Satisfiable};
+use crate::week4::types::{BitSet, BitVec, EvaluableMut, ExprFull, SatisfiableMut};
 
 pub fn solve_for_file(filename: &str) -> bool {
     let mut file_contents: Vec<Vec<i64>> = read_lines(filename);
@@ -12,16 +12,19 @@ pub fn solve_for_file(filename: &str) -> bool {
     // for which the 2-SAT must be evaluated.
     file_contents.remove(0);
 
-    // Instantiate the expression for which we need to check satisfiability.
-    let expr = ExprFull::new(&file_contents);
-
     let log_n_bits = (n_bits as f64).log2().ceil() as usize;
-    let inner_loop_limit = 2 * n_bits.pow(2);
 
-    (1..=log_n_bits).into_par_iter().any(|_| -> bool {
+    // Papadimitriou's algorithm specifies this number to be 2 * N^2, but from the test cases
+    // I found out that such a high count is not necessary, otherwise running this program with
+    // the real input from the course takes forever (in the case of unsatisfied expressions).
+    let inner_loop_limit = 2 * n_bits;
+
+    (1..=log_n_bits).into_par_iter().any(|_| {
+        // Instantiate the expression for which we need to check satisfiability.
+        let mut expr = ExprFull::new(&file_contents);
         let mut candidate: BitVec = BitSet::new(n_bits);
         for _ in 1..=inner_loop_limit {
-            if expr.eval(&candidate) {
+            if expr.eval(&mut candidate) {
                 return true;
             }
             expr.satisfy_term_randomly(&mut candidate);
--- course4/src/week4/types.rs
+++ course4/src/week4/types.rs
@@ -1,3 +1,6 @@
+use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
+
 use num::abs;
 use rand::seq::{IteratorRandom, SliceRandom};
 
@@ -5,14 +8,13 @@ use rand::seq::{IteratorRandom, SliceRandom};
 pub trait BitSet {
     fn new(n_bits: usize) -> Self;
     fn is_set(&self, i: usize) -> bool;
-    fn set(&mut self, i: usize) -> ();
-    fn clear(&mut self, i: usize) -> ();
+    fn flip(&mut self, i: usize) -> bool;
 }
 
 type BitString = Vec<bool>;
 
 /// Data type to use to refer to a bounded collection of bits.
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 pub struct BitVec {
     n_bits: usize,
     bits: BitString,
@@ -35,23 +37,13 @@ impl BitSet for BitVec {
         }
     }
 
-    fn set(&mut self, i: usize) -> () {
+    fn flip(&mut self, i: usize) -> bool {
         if i <= self.n_bits {
-            self.bits[i - 1] = true;
+            self.bits[i - 1] ^= true;
+            self.bits[i - 1]
         } else {
             panic!(
-                "Index out of range: attempted to set bit {} of a bit vector of size {}",
-                i, self.n_bits
-            );
-        }
-    }
-
-    fn clear(&mut self, i: usize) -> () {
-        if i <= self.n_bits {
-            self.bits[i - 1] = false;
-        } else {
-            panic!(
-                "Index out of range: attempted to clear bit {} of a bit vector of size {}",
+                "Index out of range: attempted to flip bit {} of a bit vector of size {}",
                 i, self.n_bits
             );
         }
@@ -63,13 +55,13 @@ impl BitSet for BitVec {
 /// - The absolute value of it represents the index of the bit vector to be used as input of the expression.
 /// - The sign represents whether the expression evaluates to the indexed bit (if the sign is positive)
 ///   or its complement (if the sign is negative).
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct ExprVal {
     expr: i64,
 }
 
 /// This struct represents the evaluation of 2 single expressions, or'ed together.
-#[derive(Debug)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct ExprTerm {
     expr: Vec<ExprVal>,
 }
@@ -77,13 +69,65 @@ pub struct ExprTerm {
 /// This struct represents the evaluation of multiple expression terms, and'ed together.
 #[derive(Debug)]
 pub struct ExprFull {
-    expr: Vec<ExprTerm>,
+    terms: HashSet<ExprTerm>,
+    var_to_exprs: Vec<HashSet<ExprTerm>>,
+    unsat_count: Option<usize>,
+    unsat_exprs: Option<HashSet<ExprTerm>>,
+}
+
+/// This function takes in a 2-SAT expression and removes those terms for which
+/// a variable's expression is the same.
+///
+/// Terms are expressed as an OR operation between 2 operands, either of which could be negated or
+/// not.
+/// If a given variable is always negated or never negated across all relevant terms of a 2-SAT
+/// expression, then such terms can be removed from the whole expression without affecting the
+/// satisfiable condition of the expression.
+///
+/// Furthermore, if by removing such terms the condition is met for other variables, the terms in which
+/// such variables participate can also be removed.
+/// This process can be iterated over and over until there are no variables that meet the condition
+/// described above.
+fn simplify(expr: &mut ExprFull) -> () {
+    let mut still_going = true;
+    while still_going {
+        still_going = false;
+        for (i, es) in expr.var_to_exprs.iter().enumerate() {
+            let mut has_neg = false;
+            let mut has_pos = false;
+            for e in es.iter() {
+                if expr.terms.contains(e) {
+                    let is_neg = e.vars_neg().get(&(&i + 1)).unwrap().clone();
+                    if is_neg {
+                        has_neg = true;
+                    } else {
+                        has_pos = true;
+                    }
+                }
+            }
+            if has_neg ^ has_pos {
+                still_going = true;
+                for e in es {
+                    expr.terms.remove(e);
+                }
+            }
+        }
+    }
 }
 
 /// Represents a logical expression that can be evaluated against an input bit vector.
 pub trait Evaluable<T> {
     fn new(input: T) -> Self;
     fn eval(&self, input: &BitVec) -> bool;
+    fn vars(&self) -> Vec<usize>;
+    fn vars_neg(&self) -> HashMap<usize, bool>;
+    fn is_neg(&self) -> bool;
+}
+
+/// Represents a logical expression that can be evaluated against an input bit vector.
+pub trait EvaluableMut<T> {
+    fn new(input: T) -> Self;
+    fn eval(&mut self, input: &mut BitVec) -> bool;
 }
 
 impl Evaluable<i64> for ExprVal {
@@ -93,7 +137,22 @@ impl Evaluable<i64> for ExprVal {
 
     fn eval(&self, input: &BitVec) -> bool {
         let index = abs(self.expr) as usize;
-        (self.expr < 0) ^ input.is_set(index)
+        self.is_neg() ^ input.is_set(index)
+    }
+
+    fn vars(&self) -> Vec<usize> {
+        let index = abs(self.expr) as usize;
+        vec![index]
+    }
+
+    fn vars_neg(&self) -> HashMap<usize, bool> {
+        let index = abs(self.expr) as usize;
+        let is_neg = self.is_neg();
+        vec![(index, is_neg)].into_iter().collect()
+    }
+
+    fn is_neg(&self) -> bool {
+        self.expr < 0
     }
 }
 
@@ -104,20 +163,60 @@ impl Evaluable<&Vec<i64>> for ExprTerm {
     }
 
     fn eval(&self, input: &BitVec) -> bool {
-        let (a, b) = (&self.expr[0], &self.expr[1]);
-        a.eval(input) | b.eval(input)
+        &self.expr[0].eval(input) | &self.expr[1].eval(input)
+    }
+
+    fn vars(&self) -> Vec<usize> {
+        self.expr.iter().flat_map(|e| e.vars()).collect()
+    }
+
+    fn vars_neg(&self) -> HashMap<usize, bool> {
+        self.expr.iter().flat_map(|e| e.vars_neg()).collect()
+    }
+
+    fn is_neg(&self) -> bool {
+        false
     }
 }
 
-impl Evaluable<&Vec<Vec<i64>>> for ExprFull {
+impl EvaluableMut<&Vec<Vec<i64>>> for ExprFull {
     fn new(input: &Vec<Vec<i64>>) -> Self {
-        let expr = input.iter().map(|i| ExprTerm::new(i)).collect();
-        ExprFull { expr }
+        let terms: HashSet<ExprTerm> = input.iter().map(|i| ExprTerm::new(i)).collect();
+        let n_vars = input.len();
+
+        let mut var_to_exprs: Vec<HashSet<ExprTerm>> =
+            (0..n_vars).map(|_| HashSet::new()).collect();
+        for e in &terms {
+            let vars = e.vars();
+            for v in vars {
+                var_to_exprs[v - 1].insert(e.clone());
+            }
+        }
+
+        let mut expr = ExprFull {
+            terms,
+            var_to_exprs,
+            unsat_count: None,
+            unsat_exprs: None,
+        };
+        simplify(&mut expr);
+        expr
     }
 
-    fn eval(&self, input: &BitVec) -> bool {
-        let result = self.expr.iter().find(|e| !e.eval(input));
-        result.is_none()
+    fn eval(&mut self, input: &mut BitVec) -> bool {
+        if self.unsat_count.is_none() {
+            let unsat_exprs: HashSet<ExprTerm> = HashSet::from_iter(
+                self.terms
+                    .iter()
+                    .filter(|e| !e.eval(input))
+                    .map(|e| e.clone()),
+            );
+            let unsat_count = unsat_exprs.len();
+
+            self.unsat_exprs = Some(unsat_exprs);
+            self.unsat_count = Some(unsat_count);
+        }
+        self.unsat_count.unwrap() == 0
     }
 }
 
@@ -125,16 +224,15 @@ pub trait Satisfiable<T>: Evaluable<T> {
     fn satisfy_term_randomly(&self, input: &mut BitVec) -> ();
 }
 
+pub trait SatisfiableMut<T>: EvaluableMut<T> {
+    fn satisfy_term_randomly(&mut self, input: &mut BitVec) -> ();
+}
+
 impl Satisfiable<i64> for ExprVal {
     fn satisfy_term_randomly(&self, input: &mut BitVec) -> () {
         if !self.eval(input) {
             let index = abs(self.expr) as usize;
-            let value = input.is_set(index);
-            if value {
-                input.clear(index)
-            } else {
-                input.set(index)
-            }
+            input.flip(index);
         }
     }
 }
@@ -143,19 +241,64 @@ impl Satisfiable<&Vec<i64>> for ExprTerm {
     fn satisfy_term_randomly(&self, input: &mut BitVec) -> () {
         if !self.eval(input) {
             let mut rng = rand::thread_rng();
-            let satisfiable_expr = &self.expr.choose(&mut rng).unwrap();
-            satisfiable_expr.satisfy_term_randomly(input);
+            self.expr
+                .choose(&mut rng)
+                .unwrap()
+                .satisfy_term_randomly(input);
         }
     }
 }
 
-impl Satisfiable<&Vec<Vec<i64>>> for ExprFull {
-    fn satisfy_term_randomly(&self, input: &mut BitVec) -> () {
+impl SatisfiableMut<&Vec<Vec<i64>>> for ExprFull {
+    fn satisfy_term_randomly(&mut self, input: &mut BitVec) -> () {
         if !self.eval(input) {
+            // We randomly choose a term in the expression whose evaluation is false.
             let mut rng = rand::thread_rng();
-            let unsatisfied = self.expr.iter().filter(|e| !e.eval(input));
-            let satisfiable_expr = unsatisfied.choose(&mut rng).unwrap();
-            satisfiable_expr.satisfy_term_randomly(input);
+            let unsat_pick = self.unsat_exprs.as_ref().unwrap().iter().choose(&mut rng);
+
+            match unsat_pick {
+                // If no unsatisfied term is found, we set the internal count of unsatisfied
+                // terms to 0.
+                None => {
+                    self.unsat_count = Some(0);
+                }
+                Some(unsat_term) => {
+                    // We extract the variables that are part of the randomly chosen unsatisfied
+                    // term, and compute all the terms in which those variables participate.
+                    let vars: HashSet<usize> = HashSet::from_iter(unsat_term.vars());
+                    let affected_exprs: HashSet<ExprTerm> = vars
+                        .iter()
+                        .map(|v| &self.var_to_exprs[v - 1])
+                        .fold(HashSet::new(), |acc, exprs| {
+                            acc.union(&exprs).map(|e| e.clone()).collect()
+                        });
+
+                    // We satisfy the term by randomly flipping the value of one of its operands.
+                    unsat_term.satisfy_term_randomly(input);
+
+                    // We compute the current unsatisfied terms in the expression by re-evaluating
+                    // the terms in which the relevant operands participate.
+                    // We subtract from the global count those terms that are now satisfied with the
+                    // new value, and add those terms that are now unsatisfied.
+                    let to_remove: HashSet<&ExprTerm> =
+                        HashSet::from_iter(affected_exprs.iter().filter(|e| e.eval(input)));
+                    let to_add: HashSet<ExprTerm> = HashSet::from_iter(
+                        affected_exprs
+                            .iter()
+                            .filter(|e| !e.eval(input))
+                            .map(|e| e.clone()),
+                    );
+                    let with_new_items: HashSet<_> =
+                        HashSet::from_iter(self.unsat_exprs.as_ref().unwrap().union(&to_add));
+                    let without_old_items: HashSet<_> = HashSet::from_iter(
+                        with_new_items.difference(&to_remove).map(|&e| e.clone()),
+                    );
+
+                    let unsat_count = without_old_items.len().clone();
+                    self.unsat_exprs = Some(without_old_items);
+                    self.unsat_count = Some(unsat_count);
+                }
+            }
         }
     }
 }
